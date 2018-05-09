1fffd04ccea56c7484b2db1cad554e4d41264d71
Andika Ahmad Ramadhan <aspendaka@gmail.com>
2018-05-10 01:15:23 +0700

    add gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const mustache = require('gulp-mustache');
 const watch = require('gulp-watch');
 const nodemon = require('gulp-nodemon');
+const clean = require('gulp-clean');
 
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
@@ -9,23 +10,28 @@ const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('views', () => {
     gulp.src(['src/views/**/*.*']).pipe(gulp.dest('build/views'));
-})
+});
 
 gulp.task('resources', () => {
     gulp.src(['src/resources/**/*.*']).pipe(gulp.dest('build/resources'));
-})
+});
 
 gulp.task('assets', function() {
     gulp.src(JSON_FILES).pipe(gulp.dest('build'));
 });
 
+gulp.task('clean', () => {
+    gulp.src('build').pipe(clean({force: true}));
+});
+
 gulp.task('watch', ['views', 'resources', 'assets'],  () => {
     return watch('src/**/*', () => {
+        gulp.src(['build']).pipe(clean({force: true}));
         gulp.src(['src/views/**/*.*']).pipe(gulp.dest('build/views'));
         gulp.src(['src/resources/**/*.*']).pipe(gulp.dest('build/resources'));
         gulp.src(JSON_FILES).pipe(gulp.dest('build'));
     });
-})
+});
 
 gulp.task('default', () => {
     console.log("Running none of gulp script")
