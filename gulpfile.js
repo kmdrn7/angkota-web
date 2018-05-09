976a3bc889a066eadb3a038170b7265f82092165
Andika Ahmad Ramadhan <aspendaka@gmail.com>
2018-05-09 18:29:51 +0700

    add some magics within gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,49 @@
 const gulp = require('gulp');
+const mustache = require('gulp-mustache');
+const watch = require('gulp-watch');
+const nodemon = require('gulp-nodemon');
+
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
-
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
-    const tsResult = tsProject.src()
-                    .pipe(tsProject());
-    return tsResult.js.pipe(gulp.dest('dist'));
-});
+gulp.task('views', () => {
+    gulp.src(['src/views/**/*.*']).pipe(gulp.dest('build/views'));
+})
 
-gulp.task('watch', ['scripts'], () => {
-    gulp.watch('src/**/*.ts', ['scripts']);
+gulp.task('resources', () => {
+    gulp.src(['src/resources/**/*.*']).pipe(gulp.dest('build/resources'));
 })
 
 gulp.task('assets', function() {
-    return gulp.src(JSON_FILES)
-    .pipe(gulp.dest('dist'));
+    gulp.src(JSON_FILES).pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['watch', 'assets']);
+gulp.task('watch',  () => {
+    return watch('src/**/*', () => {
+        gulp.src(['src/views/**/*.*']).pipe(gulp.dest('build/views'));
+        gulp.src(['src/resources/**/*.*']).pipe(gulp.dest('build/resources'));
+        gulp.src(JSON_FILES).pipe(gulp.dest('build'));
+    });
+})
+
+// gulp.task('develop', function () {
+//     var stream = nodemon({ 
+//         script: './build/index.js',
+//         ext: 'html hbs js css',
+//         ignore: ['build/'],
+//         tasks: ['assets', 'view', 'scripts', 'resources']
+//     });
+
+//     stream.on('restart', function () {
+//         console.log('restarted!')
+//     })
+//     .on('crash', function() {
+//         console.error('Application has crashed!\n')
+//         stream.emit('restart', 10)  // restart the server in 10 seconds
+//     });
+// });
+
+gulp.task('default', () => {
+    console.log("Running none of gulp script")
+});
