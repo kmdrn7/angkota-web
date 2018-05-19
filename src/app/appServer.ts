5e256b857a9d4f89eab8e8f2c4d040e6e2504788
AzureAD\MuhamamdFatih <mfatihas97@gmail.com>
2018-05-20 05:53:32 +0700

    try change oringin

--- src/app/appServer.ts
+++ src/app/appServer.ts
@@ -54,7 +54,7 @@ class AppServer {
         this.app.use(cors)
         this.server = this.app.listen(7361,'0.0.0.0')
         this.io = socketIo(this.server)
-        this.io.origins('https://jasaraharja.gravicodev.com')
+        this.io.origins('*:*')
 
         new Socket(this.io)
     }
