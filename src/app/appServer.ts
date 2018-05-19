44fa61eb5b0b38f2bd2f6c69ec4f58c8b0d3791f
AzureAD\MuhamamdFatih <mfatihas97@gmail.com>
2018-05-20 05:57:10 +0700

    try cors

--- src/app/appServer.ts
+++ src/app/appServer.ts
@@ -28,7 +28,26 @@ class AppServer {
     }
 
     config (): void {        
-        this.app.use(cors())
+        this.app.use(cors({ origin: '*' }));
+        // Settings for CORS
+        this.app.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.header('Access-Control-Allow-Origin', '*');
+
+            // Request methods you wish to allow
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', false);
+
+            // Pass to next layer of middleware
+            next();
+        });
         this.app.use(logger('dev'))
         this.app.use(bodyParser.urlencoded({ extended: false }))
         this.app.use(bodyParser.json())
